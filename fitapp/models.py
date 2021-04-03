1d21ce22d4e90dbe6f1f4ba6a0e2c00ad2d9336f
jmheilemann <jmheilemann@gmail.com>
2021-04-03 09:50:39 -0400

    progress bar fixed

--- fitapp/models.py
+++ fitapp/models.py
@@ -5,8 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class User(models.Model):
     user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
-    #on_delete = models.DO_NOTHING
-    value = models.IntegerField(default=20)
+    current = models.IntegerField(default=20)
     maximum = models.IntegerField(default=100)
 
     
