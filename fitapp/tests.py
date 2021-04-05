1f6566f1aac9de8caea1a3fa56b3595c35d6228b
jpg6qdf <72308549+jpg6qdf@users.noreply.github.com>
2021-04-05 16:02:15 -0300

    Update tests.py

--- fitapp/tests.py
+++ fitapp/tests.py
@@ -8,14 +8,18 @@ class DummyTestCase(TestCase):
     def setUp(self):
         x = 1
         # self.user_1 = User.objects.create_user(username="test", email='test@gmail.com', password='test')
-        # self.logs_1
+        self.logs_1 = Logs.objects.create(exercise="running", date="4/5/2021", duration="30 minutes", intensity="medium", area="legs")
 
     def test_dummy_test_case(self):
         self.assertEqual(1, 1)
 
-    # def test_dummy_test_case(self):
+    # def user_test_case(self):
     #     Test = User()
     #     Test.user = self.user_1
     #     print(Test.__str__())
     #     self.assertEqual(Test.__str__(), "test Profile")
 
+    def logs_test_case(self):
+        Test = self.logs_1
+        print(Test.__str__())
+        self.assertEqual(Test.__str__(), "4/5/2021" + ":" + "running" + "\n")
