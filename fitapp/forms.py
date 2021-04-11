db31fe47a650172268dfcc831ee4b7886b5eb16e
Justin LaKier <grandforksuary@gmail.com>
2021-04-11 13:09:45 -0400

    Changed to multiple choice

--- fitapp/forms.py
+++ fitapp/forms.py
@@ -1,13 +1,23 @@
 from django import forms
 from .models import Logs
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class RangeInput(forms.NumberInput):
+    input_type = 'range'
+
 class LogsForm(forms.ModelForm):
-    exercise = forms.CharField(max_length=200)#, help_text="title.")
-    date = forms.CharField(max_length=200)#, help_text="text.")      #could be slider, buttons, etc
-    duration = forms.CharField(max_length=200)#, help_text="text.")      #could be slider, buttons, etc.        ##also includes reps.
-    intensity = forms.CharField(max_length=200)#, help_text="text.")     #could be slider, buttons, etc
-    area = forms.CharField(max_length=200)#, help_text="text.")     #could be slider, buttons, etc
+#    exercise = forms.CharField(max_length=10, choices=EXERCISE_CHOICES, default='other')#, help_text="title.")
+#    date = forms.CharField(max_length=200)#, help_text="text.")      #could be slider, buttons, etc
+#    duration = forms.CharField(max_length=200)#, help_text="text.")      #could be slider, buttons, etc.        ##also includes reps.
+#    intensity = forms.CharField(max_length=200)#, help_text="text.")     #could be slider, buttons, etc
+    #area = forms.CharField(max_length=200)#, help_text="text.")     #could be slider, buttons, etc
     ## can include other relevant info we want to encourage
     class Meta:
         model = Logs
-        fields = ('exercise', 'date', 'duration', 'intensity', 'area')
+        fields = ('exercise', 'date', 'duration', 'intensity')
+        widgets = {
+            'date': DateInput(),
+            'duration': RangeInput(attrs={'max': 100})
+        }
--- fitapp/models.py
+++ fitapp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 import datetime
 from django import forms
 from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -13,13 +14,42 @@ class User(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
+EXERCISE_CHOICES = (
+    ('pushup','pushup'),
+    ('situp', 'situp'),
+    ('pullup','pullup'),
+    ('squat','squat'),
+    ('burpee','burpee'),
+    ('lift','lift'),
+    ('run','run'),
+    ('stretches','stretches'),
+    ('other','other'),
+)
+
+INTENSITY_CHOICES = (
+    ('light','light'),
+    ('moderate', 'moderate'),
+    ('vigorous','vigorous'),
+)
+
+DEFAULT_duration = 1
+MAX_duration = 100
+MIN_duration = 0
+
+#DURATION should be slider
+#DATE should be found using datepicker
+
+
 # logs model
 class Logs(models.Model):
-    exercise = models.CharField(max_length=200)#, help_text="title.")
-    date = models.CharField(max_length=200)#, help_text="text.")      #could be slider, buttons, etc
-    duration = models.CharField(max_length=200)#, help_text="text.")      #could be slider, buttons, etc.        ##also includes reps.
-    intensity = models.CharField(max_length=200)#, help_text="text.")     #could be slider, buttons, etc
-    area = models.CharField(max_length=200)#, help_text="text.")     #could be slider, buttons, etc
+    exercise = models.CharField(max_length=10, choices=EXERCISE_CHOICES, default='other')#, help_text="title.")
+    date = models.DateField()#, help_text="text.")      #could be slider, buttons, etc
+    #duration = models.CharField(max_length=200)#, help_text="text.")      #could be slider, buttons, etc.        ##also includes reps.
+    
+    duration = models.PositiveSmallIntegerField( name=('duration'), default=DEFAULT_duration,validators=[MinValueValidator(MIN_duration), MaxValueValidator(MAX_duration)])
+        #_('duration'), 
+    intensity = models.CharField(max_length=10, choices=INTENSITY_CHOICES, default='light')#, help_text="text.")     #could be slider, buttons, etc
+    #area = models.CharField(max_length=200)#, help_text="text.")     #could be slider, buttons, etc
     ## can include other relevant info we want to encourage
     def __str__(self):
         return self.date + ":" + self.exercise + "\n"
