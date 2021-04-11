1eeceaa0c236455630fb296cd3d795cc1ff7b2ce
RoboticPanda77 <natmonahan77@gmail.com>
2021-04-11 16:17:43 -0400

    Completed merge

--- fitapp/models.py
+++ fitapp/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
-from django import forms
-from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
-class User(models.Model):
-    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    level = models.IntegerField(default=0)
     current = models.IntegerField(default=0) # bar part of progress bar
     maximum = models.IntegerField(default=100) # maximum length bar can be
     def __str__(self):
@@ -39,6 +39,14 @@ MIN_duration = 0
 #DURATION should be slider
 #DATE should be found using datepicker
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 # logs model
 class Logs(models.Model):
@@ -46,13 +54,11 @@ class Logs(models.Model):
     date = models.DateField()#, help_text="text.")      #could be slider, buttons, etc
     #duration = models.CharField(max_length=200)#, help_text="text.")      #could be slider, buttons, etc.        ##also includes reps.
     
-    #duration = models.NumberInput
-    
+    duration = models.PositiveSmallIntegerField( name=('duration'), default=DEFAULT_duration,validators=[MinValueValidator(MIN_duration), MaxValueValidator(MAX_duration)])
+        #_('duration'), 
     intensity = models.CharField(max_length=10, choices=INTENSITY_CHOICES, default='light')#, help_text="text.")     #could be slider, buttons, etc
-    
-    duration = models.PositiveSmallIntegerField( name='duration', default=DEFAULT_duration,validators=[MinValueValidator(MIN_duration), MaxValueValidator(MAX_duration)])
-    
-    #area = models.CharField(max_length=200)#, help_text="text.")     #could be slider, buttons, etc
+    area = models.CharField(max_length=200)#, help_text="text.")     #could be slider, buttons, etc
     ## can include other relevant info we want to encourage
+    #owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.date + ":" + self.exercise + "\n"
