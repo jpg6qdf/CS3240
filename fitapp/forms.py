c3441f795a7c1c31bc62a3e5b9627eecd041641e
Justin LaKier <grandforksuary@gmail.com>
2021-04-19 16:36:38 -0400

    Number for forms
    trying to simplify so it passes travis

--- fitapp/forms.py
+++ fitapp/forms.py
@@ -19,7 +19,7 @@ class LogsForm(forms.ModelForm):
         fields = ('exercise', 'date', 'duration', 'intensity', 'area')
         widgets = {
             'date': DateInput(),
-            'duration': RangeInput(attrs={'max': 100})
+            'duration': forms.NumberInput(attrs={'max': '100', 'type':'range', 'step': '1', 'min': '0', 'id':'myRange'})
         }
 
 class CommentForm(forms.ModelForm):
