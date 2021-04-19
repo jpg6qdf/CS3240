b9c7401f884e54ae090ceb149b7a5309d4d85b53
RoboticPanda77 <natmonahan77@gmail.com>
2021-04-19 17:53:45 -0400

    Static updates

--- baseproject/settings.py
+++ baseproject/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
