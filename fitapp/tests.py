f84ab5d8dd7e44cc61ab78a8c4d12674bb902f7a
jpg6qdf <72308549+jpg6qdf@users.noreply.github.com>
2021-05-02 14:00:48 -0300

    Update tests.py

--- fitapp/tests.py
+++ fitapp/tests.py
@@ -10,6 +10,8 @@ class DummyTestCase(TestCase):
         self.logs_1 = Logs.objects.create(exercise="running", date="2021-04-11", duration="30", intensity="moderate")
         testuser = User.objects.create(username="test10", email="test10@gmail.com")
         self.profile_1 = Profile.objects.create(user=testuser, level=5, current=65, maximum=100)
+        self.profile_1.save()
+        self.profile_1.id
 
 
     def test_log(self):
