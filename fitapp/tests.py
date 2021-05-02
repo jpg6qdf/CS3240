d8d31237097af40afa23399f8f011bbf5c92f74e
jpg6qdf <72308549+jpg6qdf@users.noreply.github.com>
2021-05-02 15:48:44 -0300

    Update tests.py

--- fitapp/tests.py
+++ fitapp/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from fitapp.models import User, Logs, Profile
+from fitapp.models import User, Logs, Profile, Comment
 from django.contrib.auth.models import User
 import fitapp.views as views
 
@@ -9,9 +9,10 @@ class DummyTestCase(TestCase):
         x = 1
         self.logs_1 = Logs.objects.create(exercise="running", date="2021-04-11", duration="30", intensity="moderate")
         testuser = User.objects.create(username="test10", email="test10@gmail.com")
-        self.profile_1 = Profile.objects.create(user=testuser, level=5, current=65, maximum=100)
-        self.profile_1.save()
-        self.profile_1.id
+        # self.profile_1 = Profile.objects.create(user=testuser, level=5, current=65, maximum=100)
+        # self.profile_1.save()
+        # self.profile_1.id
+        self.comments_1 = Comment.objects.create(name="test", post=self.logs_1, body="test", created_on="2021-04-11", active=True)
 
 
     def test_log(self):
@@ -20,7 +21,12 @@ class DummyTestCase(TestCase):
         print(Test.__str__())
         self.assertEqual(Test.__str__(), "2021-04-11" + ":" + "running" + "\n")
 
-    def test_profile(self):
-        Test = self.profile_1
+    # def test_profile(self):
+    #     Test = self.profile_1
+    #     print(Test.__str__())
+    #     self.assertEqual(Test.__str__(), "test10 Profile")
+
+    def test_comment(self):
+        Test = self.comments_1
         print(Test.__str__())
-        self.assertEqual(Test.__str__(), "test10 Profile")
+        self.assertEqual(Test.__str__(), "Comment test by test")
