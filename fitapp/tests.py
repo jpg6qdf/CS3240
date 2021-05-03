0b60f64089f49718dd55ac978bd01b99e1027556
jpg6qdf <72308549+jpg6qdf@users.noreply.github.com>
2021-05-03 12:38:05 -0300

    Update tests.py

--- fitapp/tests.py
+++ fitapp/tests.py
@@ -9,23 +9,14 @@ class DummyTestCase(TestCase):
         x = 1
         self.logs_1 = Logs.objects.create(exercise="running", date="2021-04-11", duration="30", intensity="moderate")
         testuser = User.objects.create(username="test10", email="test10@gmail.com")
-        # self.profile_1 = Profile.objects.create(user=testuser, level=5, current=65, maximum=100)
-        # self.profile_1.save()
-        # self.profile_1.id
         self.comments_1 = Comment.objects.create(name="test", post=self.logs_1, body="test", created_on="2021-04-11", active=True)
         self.c = Client()
-
+        adminuser = User.objects.get(username="b-08-admin2")
 
     def test_log(self):
-        # self.assertEqual(1, 1)
         Test = self.logs_1
         print(Test.__str__())
         self.assertEqual(Test.__str__(), "2021-04-11" + ":" + "running" + "\n")
-
-    # def test_profile(self):
-    #     Test = self.profile_1
-    #     print(Test.__str__())
-    #     self.assertEqual(Test.__str__(), "test10 Profile")
 
     def test_comment(self):
         Test = self.comments_1
@@ -55,4 +46,10 @@ class DummyTestCase(TestCase):
     def test_leaderboardtab_beforelogin(self):
         Test = self.c
         self.assertEqual(Test.get('/fitapp/leaderboard/').status_code, 302)
+
+    def test_hometab_afterlogin(self):
+        TestClient = self.c
+        TestAdmin = self.adminuser
+        TestClient.force_login(TestAdmin)
+        self.assertEqual(Test.get('/').status_code, 200)
 
