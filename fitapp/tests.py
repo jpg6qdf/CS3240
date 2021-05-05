f3e137d2e8181c64a34530361cdab2592af27988
jpg6qdf <72308549+jpg6qdf@users.noreply.github.com>
2021-05-05 15:50:59 -0300

    Update tests.py

--- fitapp/tests.py
+++ fitapp/tests.py
@@ -107,16 +107,15 @@ class DummyTestCase(TestCase):
         Test.force_login(adminuser)
         self.assertEqual(Test.post("/fitapp/Logs/", data={"exercise": "running", "date": "2021-04-11", "duration": "30", "intensity": "moderate", "area": "legs"}).status_code, 200)
 
-    def test_logstab_submitalog2(self):
-        Test = self.c
-        adminuser = User.objects.create_superuser(username="testadmin")
-        Test.force_login(adminuser)
-        self.assertEqual(Test.post("/fitapp/Logs/", data={"exercise": "runnings", "date": "2021-042-11", "duration": "501", "intensity": "moderate", "area": "legs"}).status_code, 200)
-
     def test_commentsform(self):
         form_data={'name': 'test', 'body': 'test'}
         form = CommentForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+    def test_commentsform2(self):
+        form_data={'name': 'fjkfjdksflkdjfsdkjfldksjflfjdkslfdsfkjdslkfjdlsfkjkldsfjkdslfdkjsldfjsdlkjfdskjfldskfj', 'body': 'test'}
+        form = CommentForm(data=form_data)
+        self.assertFalse(form.is_valid())
 
     def test_profileviews(self):
         request = self.factory.get('/fitapp/progress/')
